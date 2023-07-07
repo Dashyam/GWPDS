19d778bd79b62afceaf9fa46533e91a6788b3fe3
Dashyam Madhok <dashyam@Dashyams-MacBook-Air.local>
2023-07-07 15:02:03 +0530

    Stacks

---
+++ stack.py
@@ -0,0 +1,36 @@
+# stack = [10, 20, 30, 50]
+# ans = "y"
+# length = len(stack)
+# while (ans == "y"):
+#     print("1. PUSH")
+#     print("2. POP")
+#     print("3. DISPLAY")
+#     choice = int(input("Enter your choice: "))
+#     if choice == 1:
+#         element = int(input("Enter any element:"))
+#         stack.append(element)
+#         print(stack)
+#     elif choice == 2:
+#         if stack == []:
+#             print("Stack Empty")
+#         else:
+#             print(stack.pop())
+#             print(stack)
+#     elif choice == 3:
+#         if stack == []:
+#             print("Stack empty")
+#         else:
+#             for i in range(length - 1, -1, -1):
+#                 print(stack[i], end=" ")
+#     ans = input("\nDo you want to continue: ")
+
+# vowels = ["a", "e", "i", "o", "u"]
+# word = input("Enter the word to be searched:")
+# stack = []
+# for letter in word:
+#     if letter in vowels:
+#         if letter not in stack:
+#             stack.append(letter)
+# print(stack)
+# print("The number of vowels in",word,"are",len(stack))
+print("Absbcbcgdbc")
